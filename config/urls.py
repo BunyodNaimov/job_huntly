a914308b54fbd9662403d0af66c76b5f76eda620
bunyod <naimov.bunyod2021@gmail.com>
2023-07-30 07:46:09 +0300

    update project

--- config/urls.py
+++ config/urls.py
@@ -14,8 +14,9 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.conf import settings
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path, include, re_path
 
 from rest_framework import permissions
 from drf_yasg.views import get_schema_view
@@ -44,3 +45,8 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('api-auth/', include('rest_framework.urls'))
 ]
+
+urlpatterns += swagger_urls
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
