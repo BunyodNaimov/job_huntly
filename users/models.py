4953403f0a1851a6d35294e39ffaf50e1a6c8d14
bunyod <naimov.bunyod2021@gmail.com>
2023-07-31 10:47:21 +0300

    update project

--- users/models.py
+++ users/models.py
@@ -5,8 +5,9 @@ from django.db import models
 class CustomUser(AbstractUser):
     email = models.EmailField(max_length=255, unique=True)
     username = models.CharField(unique=True, max_length=255, null=True, blank=True)
-    phone = models.PositiveIntegerField(max_length=15, null=True, blank=True)
+    phone = models.CharField(max_length=15, null=True, blank=True)
 
     USERNAME_FIELD = "email"
+    REQUIRED_FIELDS = []
 
 
