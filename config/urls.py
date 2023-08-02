1df166ae0c2b35decb371bed9e798b63c9ee2b30
Odiljon11 <odiljontoirjonov350@gmail.com>
2023-08-02 16:46:27 +0500

    fix vanancy

--- config/urls.py
+++ config/urls.py
@@ -45,7 +45,7 @@ swagger_urls = [
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api-auth/', include('rest_framework.urls')),
-    path('vanancy/', include('vacancy.urls'), name='vacancy')
+    path('vacancy/', include('vacancy.urls'), name='vacancy')
 ]
 
 urlpatterns += swagger_urls
